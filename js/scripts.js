a4db2bec602a33287546dc739b72da5f14416bda
David Hawkins <uxhawk@gmail.com>
2020-03-16 15:58:43 -0400

    added google places API calls

--- js/scripts.js
+++ js/scripts.js
@@ -1,58 +1,59 @@
-
 $(document).ready(function() {
 
     $("#search-btn").click(function() {
-        var city = $("#city-val").val();
+        var city = $("#pac-input").val();
         if (city === "") {
-            alert("Please enter a name for the city.")
+            return
         }
-        console.log(city);
+
+        var request = {
+            query: city
+        };
+
+        var service = new google.maps.places.PlacesService(document.getElementById("service"));
+        service.textSearch(request, callback);
 
         //clear after search
-        $("#city-val").val("");
+        $("#pac-input").val("");
 
 
-    
-var search = "Arlington";
-var queryURL = "https://developers.zomato.com/api/v2.1/search?entity_type=" + search + "&q=baltimore&count=10&sort=rating&order=asc"
-//curl -X GET --header "Accept: application/json" --header "user-key: bf03c64be8ee3a6da3d0cb9bfd69e5e3" "https://developers.zomato.com/api/v2.1/cities?q=seattle"
 
-    $.ajax({
-        url: queryURL,
-        method: "GET",
-        headers: {
-            "user-key": "bf03c64be8ee3a6da3d0cb9bfd69e5e3"
-        }
-      }).then(function(response) {
-          console.log(response);
-      });
-      
-        // var queryURL;
-
-        // $.ajax({
-        //     url: queryURL,
-        //     method: "GET"
-        // }).then(function(response) {
-        //     $("#topArticles").empty();
-        //     var results = response;
-        //     console.log(searchTermVal);
-        //     console.log(results);
-
-        //     for (let i = 0; i < numArticlesVal; i++) {
-        //         var newDiv = $("<div>");
-        //         var pAbs = $("<p>");
-        //         pAbs.text(results.response.docs[0].abstract)
-        //         var pTitle = $("<p>");
-        //         var pPubYear = $("<p>");
+        // var search = "Arlington";
+        var queryURL = "https://developers.zomato.com/api/v2.1/search?entity_type=city&q=" + city + "&count=10";
+        //curl -X GET --header "Accept: application/json" --header "user-key: bf03c64be8ee3a6da3d0cb9bfd69e5e3" "https://developers.zomato.com/api/v2.1/cities?q=seattle"
 
 
-        //     }
-        // });
-        //end of ajax
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+            headers: {
+                "user-key": "bf03c64be8ee3a6da3d0cb9bfd69e5e3"
+            }
+        }).then(function(response) {
+            console.log(response);
+        });
+
+
     });
+
+    //places auto complete
+    var input = document.getElementById("pac-input");
+    var options = {
+        types: ['(cities)'],
+    };
+    var autocomplete = new google.maps.places.Autocomplete(input, options);
+
+    function callback(results, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+
+            var lat = results[0].geometry.location.lat();
+            var lng = results[0].geometry.location.lng();
+
+            console.log(lat, lng);
+        }
+    }
 
 
 
 
 });
-
