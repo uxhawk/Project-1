026b8e101b0cc5db74a98e6bd1cfc68d76138c04
David Hawkins <uxhawk@gmail.com>
2020-03-16 20:25:03 -0400

    var autocomplete is instantiated before the callback function

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
     var favorites = [];
     var city, lat, lng;
     var apiSelector;
-  var autocomplete = new google.maps.places.Autocomplete(input, options);
 
     //click event listener for event toggles
     $(document).on("click", ".toggle", function() {
@@ -18,36 +17,36 @@ $(document).ready(function() {
     var lng;
 
 
-     //places auto complete
-     var input = document.getElementById("pac-input");
-     var options = {
-         types: ['(cities)'],
-     };
-     
- 
-     function callback(results, status) {
-         if (status == google.maps.places.PlacesServiceStatus.OK) {
- 
-             lat = results[0].geometry.location.lat();
-             lng = results[0].geometry.location.lng();
- 
-             console.log(lat, lng);
+    //places auto complete
+    var input = document.getElementById("pac-input");
+    var options = {
+        types: ['(cities)'],
+    };
 
-             var queryURL = "https://developers.zomato.com/api/v2.1/search?count=10&lat=" + lat + "&lon=" + lng + "&sort=rating&order=asc";     
-     
-             $.ajax({
-                 url: queryURL,
-                 method: "GET",
-                 headers: {
-                     "user-key": "bf03c64be8ee3a6da3d0cb9bfd69e5e3"
-                 }
-             }).then(function(response) {
-                 console.log(response);
-             });
-         }
-     }
- 
-     
+
+    function callback(results, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+
+            lat = results[0].geometry.location.lat();
+            lng = results[0].geometry.location.lng();
+
+            console.log(lat, lng);
+
+            var queryURL = "https://developers.zomato.com/api/v2.1/search?count=10&lat=" + lat + "&lon=" + lng + "&sort=rating&order=asc";
+
+            $.ajax({
+                url: queryURL,
+                method: "GET",
+                headers: {
+                    "user-key": "bf03c64be8ee3a6da3d0cb9bfd69e5e3"
+                }
+            }).then(function(response) {
+                console.log(response);
+            });
+        }
+    }
+
+
 
     $("#search-btn").click(function() {
         city = $("#pac-input").val();
