9dbbcfbc09a320e351105cb34d38b50f27b9202f
Frank <fcalderon3291@gmail.com>
2020-03-16 20:03:56 -0400

    Food location call is now working using Long & Lat

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,40 @@
 $(document).ready(function() {
 
+    var lat;
+    var lng;
+
+
+     //places auto complete
+     var input = document.getElementById("pac-input");
+     var options = {
+         types: ['(cities)'],
+     };
+     var autocomplete = new google.maps.places.Autocomplete(input, options);
+ 
+     function callback(results, status) {
+         if (status == google.maps.places.PlacesServiceStatus.OK) {
+ 
+             lat = results[0].geometry.location.lat();
+             lng = results[0].geometry.location.lng();
+ 
+             console.log(lat, lng);
+
+             var queryURL = "https://developers.zomato.com/api/v2.1/search?count=10&lat=" + lat + "&lon=" + lng + "&sort=rating&order=asc";     
+     
+             $.ajax({
+                 url: queryURL,
+                 method: "GET",
+                 headers: {
+                     "user-key": "bf03c64be8ee3a6da3d0cb9bfd69e5e3"
+                 }
+             }).then(function(response) {
+                 console.log(response);
+             });
+         }
+     }
+ 
+     
+
     $("#search-btn").click(function() {
         var city = $("#pac-input").val();
         if (city === "") {
@@ -16,44 +51,13 @@ $(document).ready(function() {
         //clear after search
         $("#pac-input").val("");
 
-
-
-        // var search = "Arlington";
-        var queryURL = "https://developers.zomato.com/api/v2.1/search?entity_type=city&q=" + city + "&count=10";
-        //curl -X GET --header "Accept: application/json" --header "user-key: bf03c64be8ee3a6da3d0cb9bfd69e5e3" "https://developers.zomato.com/api/v2.1/cities?q=seattle"
-
-
-        $.ajax({
-            url: queryURL,
-            method: "GET",
-            headers: {
-                "user-key": "bf03c64be8ee3a6da3d0cb9bfd69e5e3"
-            }
-        }).then(function(response) {
-            console.log(response);
-        });
+        
+      
 
 
     });
 
-    //places auto complete
-    var input = document.getElementById("pac-input");
-    var options = {
-        types: ['(cities)'],
-    };
-    var autocomplete = new google.maps.places.Autocomplete(input, options);
-
-    function callback(results, status) {
-        if (status == google.maps.places.PlacesServiceStatus.OK) {
-
-            var lat = results[0].geometry.location.lat();
-            var lng = results[0].geometry.location.lng();
-
-            console.log(lat, lng);
-        }
-    }
-
-
+   
 
 
 });
