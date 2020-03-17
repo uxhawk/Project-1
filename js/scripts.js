7845285a165fa4edd5c79cb6ffd41e57f39a82ab
David Hawkins <uxhawk@gmail.com>
2020-03-17 16:16:33 -0400

    removed unncessary functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
     var favorites = [];
-    var city, lat, lng;
-    var apiSelector;
+    var city, lat, lng, apiSelector, lat, lng;
 
     $("#results-table").hide();
     //click event listener for event toggles
@@ -13,40 +12,11 @@ $(document).ready(function() {
         console.log(apiSelector);
     });
 
-
-    var lat;
-    var lng;
-
-
     //places auto complete
     var input = document.getElementById("pac-input");
     var options = {
         types: ['(cities)'],
     };
-
-
-    function callback(results, status) {
-        if (status == google.maps.places.PlacesServiceStatus.OK) {
-
-            lat = results[0].geometry.location.lat();
-            lng = results[0].geometry.location.lng();
-
-            console.log(lat, lng);
-
-            var queryURL = "https://developers.zomato.com/api/v2.1/search?count=10&lat=" + lat + "&lon=" + lng + "&sort=rating&order=desc";
-
-            $.ajax({
-                url: queryURL,
-                method: "GET",
-                headers: {
-                    "user-key": "bf03c64be8ee3a6da3d0cb9bfd69e5e3"
-                }
-            }).then(function(response) {
-                console.log(response);
-            });
-        }
-    }
-
 
 
     $("#search-btn").click(function() {
@@ -80,6 +50,7 @@ $(document).ready(function() {
 
             lat = results[0].geometry.location.lat();
             lng = results[0].geometry.location.lng();
+            console.log(lat, lng);
 
             var queryURL = "https://developers.zomato.com/api/v2.1/search?count=10&lat=" + lat + "&lon=" + lng + "&sort=rating&order=asc";
 
