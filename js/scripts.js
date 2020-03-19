94d04e4ca3f62fcf41e893015d4e1b7f6aa0d6aa
Frank <fcalderon3291@gmail.com>
2020-03-18 22:02:16 -0400

    Added ticketmaster api

--- js/scripts.js
+++ js/scripts.js
@@ -149,6 +149,18 @@ $(document).ready(function() {
 
 
             });
+            
+            var eventURL = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=nlha7A86hmR12YOlaa17AwPtVyVRCa0B&latlong="+ lat + "," + lng
+
+            $.ajax({
+                url: eventURL,
+                method: 'GET',
+                async: true,
+                dataType: "json",
+            }).then(function(response) {
+                console.log(response);
+            
+            });
 
             //all ajax functions should be above this last curly bracket
         }
